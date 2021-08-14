372a214d842ee30c020e1ad99e86e7c95f49fd6e
krypto69 <metallipax@hotmail.com>
2021-08-14 00:57:47 -0300

    meu projeto

---
+++ src/main/java/Model/Cliente.java
@@ -0,0 +1,77 @@
+package Model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    public Cliente(Long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cliente cliente = (Cliente) o;
+
+        return id != null ? id.equals(cliente.id) : cliente.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/controller/ControllerCliente.java
@@ -0,0 +1,29 @@
+package controller;
+
+import Model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import repository.ClienteRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ControllerCliente {
+
+    @Autowired
+    public ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> listar() {
+       return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+       return clienteRepository.save(cliente);
+    }
+
+}
